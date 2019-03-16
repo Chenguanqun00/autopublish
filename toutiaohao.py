d4cc79f9118523bbe2ee59587cae88e6709f5af7
keson1 <694314541@qq.com>
2019-03-16 22:15:16 +0800

    头条自动发布图片内容

--- toutiaohao.py
+++ toutiaohao.py
@@ -6,7 +6,6 @@
 @Time: 2019年03月11日23时
 '''
 
-
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -18,13 +17,16 @@ import time
 import requests
 from authcode import math_img
 import os
+import shutil
 
+#下载
 def getpic(path, url):
     res = requests.get(url)
     with open(path, 'wb') as file:
         file.write(res.content)
         file.flush()
 
+#生成运动轨迹列表
 def get_track(distance):
     track = []
     current = 0
@@ -43,42 +45,57 @@ def get_track(distance):
           track.append(round(move))
     return track
 
-def start(image, target, url, username, password):
-    global imageurl, targeturl
-    #driver = webdriver.Chrome(r'D:\Program Files\CentBrowser\CentBrowser\Application\chromedriver.exe')
-    driver = webdriver.Chrome(r'C:\Users\ADMIN\AppData\Local\CentBrowser\Application\chromedriver.exe')
+#登陆
+def login(url, username, password):
+    driver = webdriver.Chrome(r'D:\Program Files\CentBrowser\CentBrowser\Application\chromedriver.exe')
+    #driver = webdriver.Chrome(r'C:\Users\ADMIN\AppData\Local\CentBrowser\Application\chromedriver.exe')
     driver.get(url)
     driver.maximize_window()
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(10)
+
     #跳转到账号密码登录
-    getaccount=driver.find_element_by_id('login-type-account')
-    getaccount.click()
+    driver.find_element_by_id('login-type-account').click()
 
     input1 = driver.find_element_by_id('user-name')
     input2 = driver.find_element_by_id('password')
     input1.send_keys(username)
     input2.send_keys(password)
     time.sleep(0.2)
-    #获取打开滑块验证码页面的元素
-    getcheck=driver.find_element_by_id('bytedance-login-submit')
-    #点击进入滑块验证码页面
-    getcheck.click()
-    time.sleep(3)
+
+    #获取打开滑块验证码页面的元素点击进入滑块验证码页面
+    driver.find_element_by_id('bytedance-login-submit').click()
+    time.sleep(5)
+
+    return driver
+
+# 获取验证码
+def get_authcode(driver, target, image):
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    #获取验证码的下载地址
     imageurl = soup.find(attrs={"id": "validate-big"}).get("src")
     targeturl = soup.find(attrs={"class": "validate-block"}).get("src")
     getpic(image, imageurl)
     getpic(target, targeturl)
-    return driver
 
-def track(driver, distance):
-    slideblock = driver.find_element_by_class_name("drag-button")
-    #ActionChains(driver).click_and_hold(slideblock).perform()
-    track_list=get_track(distance+3)
+#执行滑动实现登陆
+def track(driver, image, target, value, url):
+
+    # 获取验证码
+    get_authcode(driver, target, image)
+
+    # 获取需要滑动的距离
+    distance = math_img(image, target, value)
+    # 如果距离为0则重新打开
+    if distance == 0:
+        driver.close()
+        driver = login(url, username, password)
+        get_authcode(driver, target, image)
+
+    track_list = get_track(distance+3) #距离多加3，模拟人工操作
     time.sleep(2)
+    slideblock = driver.find_element_by_class_name("drag-button")
     ActionChains(driver).click_and_hold(slideblock).perform()
     time.sleep(0.2)
+
     # 根据轨迹拖拽圆球
     for track in track_list:
         ActionChains(driver).move_by_offset(xoffset=track,yoffset=0).perform()
@@ -86,7 +103,7 @@ def track(driver, distance):
     imitate=ActionChains(driver).move_by_offset(xoffset=-1, yoffset=0)
     time.sleep(0.015)
     imitate.perform()
-    time.sleep(random.randint(6,10)/10)
+    time.sleep(random.randint(6, 10)/10)
     imitate.perform()
     time.sleep(0.04)
     imitate.perform()
@@ -97,79 +114,135 @@ def track(driver, distance):
     time.sleep(0.033)
     ActionChains(driver).move_by_offset(xoffset=1, yoffset=0).perform()
     # 放开圆球
-    ActionChains(driver).pause(random.randint(6,14)/10).release(slideblock).perform()
+    ActionChains(driver).pause(random.randint(6, 14)/10).release(slideblock).perform()
     time.sleep(2)
     #务必记得加入quit()或close()结束进程，不断测试电脑只会卡卡西
     #driver.close()
 
-image = "image.jpg"
-target = "target.png"
-value = 0.7
-username = "18215565025"
-password = "Wanglihong123"
-url = "https://sso.toutiao.com/login/?service=https://mp.toutiao.com/sso_confirm/?redirect_url=JTJG"
-driver = start(image, target, url, username, password)
-distance = math_img(image, target, value)
-while distance == 0:
-    driver.close()
-    driver = start(image, target, url, username, password)
-    distance = math_img(image, target, value)
-print(distance)
-track(driver, distance)
+#遍历目录下所以文件
+def gci(path):
+    """this is a statement"""
+    parents = os.listdir(path)
+    for parent in parents:
+        if parent == "forgifs" or parent == "hilariousgifs":
+            pass
+        else:
+            child = os.path.join(path,parent)
+            #print(child)
+            if os.path.isdir(child):
+                gci(child)
+            else:
+                filepath.append(child)
+                #print(child)
 
-# cookies1 = driver.get_cookies()
-# cookies = {}
-# for i in cookies1:
-#     cookies[i["name"]] = i["value"]
-# url = "https://mp.toutiao.com/tools/upload_picture/?type=ueditor&pgc_watermark=1&action=uploadimage&encode=utf-8"
-# headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36", "Referer": "https://mp.toutiao.com/profile_v3/graphic/publish"}
-# type = "image/jpeg"
-# with open("image.jpg", "rb") as file:
-#     upfile = file.read()
-# res = requests.post(url, data={"type": "image/jpeg", "upfile": upfile}, headers=headers, cookies=cookies)
-# print(res.status_code)
-# print(res.content)
-#点击发布
-time.sleep(9)
-driver.refresh()
-time.sleep(5)
-driver.find_elements_by_class_name('tui2-menu-item')[1].click()
-time.sleep(4)
+#获取新文件
+def get_newfile(count):
+    gci(path)
+    filelist = []
+    imgs = {}
+    for i in range(count):
+        oldfile = filepath.pop(random.randint(0, len(filepath) - 1))
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
+        newfile = os.path.join(newpath, os.path.basename(oldfile))
+        filelist.append(newfile)
+        shutil.copyfile(oldfile, newfile)
+        print("已复制新文件：%s" % newfile)
+        os.remove(oldfile)
+        print("已删除旧文件：%s" % oldfile)
+    for file in filelist:
+        filename = os.path.basename(file)
+        if filename.endswith('.gif'):
+            name = filename.split('.')[0]
+            imgs[file] = name
+    return imgs
+
 #上传图片
-driver.find_element_by_class_name('ql-image').click()
-time.sleep(1)
-#选择图片
-dirname = r"C:\Users\ADMIN\Desktop\code\autopublish"
-imgs = {}
-for file in os.listdir(dirname):
-    if file.endswith('.gif'):
-        name = file.split('.')[0]
-        img = dirname + '\\' + file
-        imgs[img] = name
-i = 1
-names = []
-for img in imgs:
-    print('i=' + str(i))
-    driver.find_element_by_xpath("//span[@class='syl-img-upload']/input").send_keys(img)
-    wait = WebDriverWait(driver, 20)
-    try:
-        flag = wait.until(EC.presence_of_element_located((By.XPATH, '//ul[@class="image-list"]/li[' + str(i) + ']/div[@class="success"]')))
-        print('success')
-        names.append(imgs[img])
-    except Exception as e:
-        print("上传失败，路径：%s,报错信息：%s" % (img, str(e)))
-    i += 1
-    #driver.find_element_by_xpath("//span[@class='syl-img-upload']/input").send_keys(r'C:\Users\Administrator\Desktop\code\autopublish\toobig.gif')
-time.sleep(5)
-driver.find_element_by_xpath("//button[@data-e2e='imageUploadConfirm-btn']").click()
-time.sleep(10)
-m = 1
-print(names)
-for name in names:
-    print('m=' + str(m))
-    print(name)
-    for n in name:
-        print(n)
-        driver.find_element_by_xpath('//div[@class="ql-editor"]/tt-image[' + str(m) + ']/div/div[@class="pgc-img-caption-wrapper"]/input').send_keys(n)
+def uploadpic(imgs):
+    i = 1
+    names = []
+    for img in imgs:
+        driver.find_element_by_xpath("//span[@class='syl-img-upload']/input").send_keys(img)
+        wait = WebDriverWait(driver, 20)
+        try:
+            flag = wait.until(EC.presence_of_element_located(
+                (By.XPATH, '//ul[@class="image-list"]/li[' + str(i) + ']/div[@class="success"]')))
+            print('success:%s' % img)
+            names.append(imgs[img])
+        except Exception as e:
+            print("上传失败，路径：%s,报错信息：%s" % (img, str(e)))
+        i += 1
+    time.sleep(5)
+    driver.find_element_by_xpath("//button[@data-e2e='imageUploadConfirm-btn']").click()
+    return names
+
+#给图片加名字
+def uploadname():
+    m = 1
+    for name in names:
+        if m > 1:
+            name = "1" + name
+        for n in name:
+            driver.find_element_by_xpath('//div[@class="ql-editor"]/tt-image[' + str(
+                m) + ']/div/div[@class="pgc-img-caption-wrapper"]/input').send_keys(n)
         time.sleep(1)
-    m += 1
+        m += 1
+
+if __name__ == '__main__':
+    image = "image.jpg"            #验证码大图
+    target = "target.png"          #验证小图
+    value = 0.7                    #验证吗识别阈值
+    username = "18215565025"       #用户名
+    password = "Wanglihong123"     #密码
+    url = "https://sso.toutiao.com/login/?service=https://mp.toutiao.com/sso_confirm/?redirect_url=JTJG"    #登陆地址
+    path = r"E:\ziyuan\gif"        #资源路径
+    newpath = r"E:\自媒体\已发表\GIF\{0}\{1}".format(time.strftime("%Y%m%d", time.localtime()), "头条号")    #新图片存放路径
+    filepath = []                  #保存新图片路径
+    count = 10                     #筛选图片数量
+
+    #打开登陆页面获取验证码
+    print("登陆中...")
+    driver = login(url, username, password)
+
+    #滑动验证并登陆
+    print("滑动验证中...")
+    track(driver, image, target, value, url)
+
+    #点击发布按钮
+    time.sleep(9)
+    driver.find_elements_by_class_name('tui2-menu-item')[1].click()
+
+    #点击上传
+    time.sleep(4)
+    driver.find_element_by_class_name('ql-image').click()
+
+    #从本地资源中随机选择图片
+    time.sleep(2)
+    print("筛选图片...")
+    imgs = get_newfile(count)
+
+    #上传图片
+    print("上传图片...")
+    names = uploadpic(imgs)
+
+    #随机从名字中生成一个标题
+    print("添加标题...")
+    title = "搞笑GIF：" + names[random.randint(0, len(names)-1)]
+    #加标题
+    driver.find_element_by_xpath('//div[@class="syl-editor-input"]/span/input').send_keys(title)
+    time.sleep(5)
+
+    #给图片加上名字
+    print("加名字...")
+    uploadname()
+
+    #滑动到页面底部
+    js = "var q=document.documentElement.scrollTop=100000"
+    driver.execute_script(js)
+
+    #选择封面设置为自动
+    driver.find_element_by_xpath('//div[@class="article-cover"]/div[@class="tui2-radio-group"]/label[3]/div/input').click()
+
+    #发表
+    time.sleep(2)
+    driver.find_element_by_id("publish").click()
